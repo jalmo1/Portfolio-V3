bea62c8fbefc415d18cda012dd6afaedb097bdf1
jalmo1 <joshuarosamktp4@gmail.com>
2022-12-20 21:21:25 -0500

    Add what I offer section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
-import { AiFillGithub, AiFillLinkedin} from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png"
+import devjosh from "../public/joshuarosa.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 export default function Home() {
   return (
@@ -32,19 +35,69 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">Joshua Almodovar</h2>
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Joshua Almodovar
+            </h2>
             <h3 className="text-2xl py-2">Front end Developer</h3>
             <p className="text-md py-5 leading-8 text-gray-800">
-              I am growing my programming skills and learning new things daily! Welcome to my portfolio
-              and please reach out if you would like to collab!
+              I am growing my programming skills and learning new things daily!
+              Welcome to my portfolio and please reach out if you would like to
+              collaborate!
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
             <AiFillGithub />
             <AiFillLinkedin />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={deved} layout="fill" objectFit="cover" />
+          <div className="relative mx-auto rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={devjosh} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+
+        <section className="min-h-screen py-3">
+          <div className="text-center py-1">
+            <h3 className="text-3xl py-1">What I offer</h3>
+            <p className="text-md leading-8 text-gray-800">
+              In the beginning of my developer journey, I was learning Python to
+              build back end servers and now I mainly use Javascript and React
+              to create beautiful web applications.
+              <br />I have experience with working with big
+              <span className="text-teal-500"> corporations</span> and also
+              collaborating on
+              <span className="text-teal-500"> open source</span> projects.
+            </p>
+          </div>
+          <div className="text-center py-5">
+            <Image
+              src={design}
+              width={100}
+              height={100}
+              className="mx-auto py-5"
+            />
+            <p>
+              I always strive to make my projects look modern with an strong
+              emphasis on designing UX/UI forward applications.
+            </p>
+            <Image
+              src={code}
+              width={100}
+              height={100}
+              className="mx-auto py-5"
+            />
+            <p>
+              Writing clean semantic code is something I strive to perfect
+              daily. Click my github link to checkout my projects and code.{" "}
+            </p>
+            <Image
+              src={consulting}
+              width={100}
+              height={100}
+              className="mx-auto py-5"
+            />
+            <p>
+              I am always willing to help anyone in need. I also like to consult
+              on projects and collaborate on projects.
+            </p>
           </div>
         </section>
       </main>
